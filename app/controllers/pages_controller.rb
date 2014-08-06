2199ad9b48095d82f66f6b51c539987353abb8fb
Gilbert <mont-phil@hotmail.fr>
2014-08-05 23:07:37 -0400

    Error page setup 400 422 500

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+
+
+
+#   ======================================ERRORS===================================
+  def show
+    render status_code.to_s, :status => status_code
+  end
+
+  protected
+
+  def status_code
+    params[:code] || 500
+  end
+
+#   ====================================ERRORS END=================================
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root "pages#home"
 
+#   =======================ERRORS=======================
+  %w( 404 422 500 ).each do |code|
+    get code, :to => "errors#show", :code => code
+  end
+
+#   =======================ERRORS END ==================
+
 
 end
