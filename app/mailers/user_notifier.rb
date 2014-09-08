493fcd7f2e5d94505ca30e8326cec486f8a2935b
Gilbert <mont-phil@hotmail.fr>
2014-09-08 14:52:26 -0400

    email delivery fixed

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,13 +1,11 @@
 class UserNotifier < ActionMailer::Base
   default from: "from@example.com"
 
-  def send_purchase_confirmation(user,admin)
+  def send_purchase_confirmation(user)
     @user = user
-    @admin = admin
+    
     mail( :to => @user.email,
           :subject => 'Thanks for your purchase!' )
 
-    mail( :to => @admin,
-          :subject => 'a new email comming!' )
   end
 end
